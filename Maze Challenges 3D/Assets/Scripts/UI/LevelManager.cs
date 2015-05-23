455572e3b4fbabc7da6b676594ef88c46e8f98df
r0b0t3d <tuanluong.it@gmail.com>
2015-05-23 13:39:51 +0700

    Loading screen

---
+++ Maze Challenges 3D/Assets/Scripts/UI/LevelManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d1cacf5fe90c3463f934e304adcfb72089a576b688e49238e970376df2d91ce2
+size 612
---
+++ Maze Challenges 3D/Assets/Scripts/UI/UIManagerScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c4f4e031462057e4ec76cc1110916b85fb0e4bb6ebe90e444fe6a6499ebf034
+size 928
