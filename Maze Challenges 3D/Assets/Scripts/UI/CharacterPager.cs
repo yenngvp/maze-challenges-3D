bdcd6fa28db47769bb0ef24b3ce88b12da192bcb
r0b0t3d <tuanluong.it@gmail.com>
2015-06-11 22:59:02 +0700

    Store view

---
+++ Maze Challenges 3D/Assets/Scripts/UI/CharacterPager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e67e5d459e3f15a317508bd9a126d64f7b50777fc245635ac35d49e69bcf46f
+size 1317
--- Maze Challenges 3D/Assets/Scripts/UI/UIManagerScript.cs
+++ Maze Challenges 3D/Assets/Scripts/UI/UIManagerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecf49cbda4344a9b7560a0c93c902168ad70dd78f5d22f8760bda3c839b739e3
-size 2657
+oid sha256:b685446c47d0730bc8b462ccb9f3acae1bd7013fa600602f249d227d9c698e8c
+size 3013
